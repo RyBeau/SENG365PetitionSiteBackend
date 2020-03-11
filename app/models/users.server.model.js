5743a7a3fc39fd9a2bb1739ecffa4fed6696fc95
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-03-11 22:13:08 +1300

    Potentially solved problem with db connection. Only call getPool() not getPool().getConnection().

--- app/models/users.server.model.js
+++ app/models/users.server.model.js
@@ -2,7 +2,7 @@ const db = require('../../config/db');
 
 exports.checkEmail = async function (email) {
     let approveEmail = false;
-    const connection = await db.getPool().getConnection();
+    const connection = await db.getPool();
     const q = "select count(*) as count from User where email = (?)";
     const [result, _] = await connection.query(q, email);
     if(result[0].count === 0) {
@@ -13,35 +13,35 @@ exports.checkEmail = async function (email) {
 
 exports.insert = async function (name, email, password, city, country) {
     const values = [name, email, password, city, country];
-    const connection = await db.getPool().getConnection();
+    const connection = await db.getPool();
     const q = "insert into User (name, email, password, city, country) values (?)";
     const [result, _] = await connection.query(q, [values]);
     return result;
 };
 
 exports.getPass = async function (email) {
-    const connection = await db.getPool().getConnection();
+    const connection = await db.getPool();
     let q = "select user_id, password from User where email = (?)";
     const [result, _] = await connection.query(q, email);
     return result;
 }
 
 exports.setAuth = async function (email, token) {
-    const connection = await db.getPool().getConnection();
+    const connection = await db.getPool();
     const values = [token, email];
     const q = "UPDATE User SET auth_token = (?) where email = (?)";
     const [result, _] = await connection.query(q, values);
 };
 
 exports.checkAuthUserId = async function (user_id) {
-    const connection = await db.getPool().getConnection();
+    const connection = await db.getPool();
     const q = "SELECT auth_token from User where user_id = (?)";
     const [result,_] = await connection.query(q, user_id);
     return result;
 };
 
 exports.removeAuth = async function (auth_token) {
-    const connection = await  db.getPool().getConnection();
+    const connection = await  db.getPool();
     const q = "UPDATE User SET auth_token = NULL where auth_token = (?)";
     const [result, _] = await connection.query(q, auth_token);
     return result;
