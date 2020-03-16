1c9be15564d0dd0c9a681a6f752bcde91f47fb4f
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-03-16 20:51:58 +1300

    added await to async call

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -167,7 +167,7 @@ exports.viewPhoto = async function (req, res) {
         const user_id = req.params.user_id;
         const filename = (await User.getPhoto(user_id))[0].photo_filename;
         if(filename != null){
-             const file = fs.readFile(process.cwd() + "/storage/photos/" + filename, "binary", (err) => {if(err)console.log(err);});
+             const file = await fs.readFile(process.cwd() + "/storage/photos/" + filename, "binary", (err) => {if(err)console.log(err);});
             res.writeHead(200, {'Content-Type': 'image/jpeg'})
                 .send(file);
         } else {
