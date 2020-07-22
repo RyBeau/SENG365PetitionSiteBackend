e5f773f73e76b306caabae9047bf64b6028bf9de
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-07-23 11:35:27 +1200

    Added cors

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const { allowCrossOriginRequestsMiddleware } = require('../app/middleware/cors.middleware');
 
 
@@ -14,6 +15,7 @@ module.exports = function () {
     app.use(bodyParser.raw({ type: 'text/plain', limit: '50mb' }));  // for the /executeSql endpoint
     app.use(bodyParser.raw({ type: 'image/*', limit: '50mb' }));
 
+    app.use(cors());
 
     // DEBUG (you can remove these)
     app.use((req, res, next) => {
